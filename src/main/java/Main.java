f9aa18157083c56659ef38ca4b3ed2598563fdae
gsbaker <gsbaker2000@icloud.com>
2021-02-19 12:58:07 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label label = new Label("Hello World");
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(label);
+        Scene scene = new Scene(stackPane, 400, 400);
+        stage.setScene(scene);
+        stage.setTitle("Hello World");
+        stage.show();
+    }
+}
