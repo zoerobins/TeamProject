c49af203964129a0b35ebd317c0d78341b2d0fbc
Nicolas Poffley <nicolaspoffley@Nicolass-MacBook-Pro.local>
2021-03-05 19:41:26 +0000

    created GameTest

--- src/test/java/org/nightshade/gamelogic/GameTest.java
+++ src/test/java/org/nightshade/gamelogic/GameTest.java
@@ -1,5 +1,33 @@
 package org.nightshade.gamelogic;
 
-class GameTest {
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.nightshade.renderer.Renderer;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+@ExtendWith(ApplicationExtension.class)
+public class GameTest {
+
+    Renderer renderer;
+    Cloud cloud;
+    Game game;
+    Stage tempStage;
+
+    @Start
+    public void start(Stage stage) {
+        game=new Game();
+        tempStage=stage;
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Hello World");
+    }
+
 
 }
