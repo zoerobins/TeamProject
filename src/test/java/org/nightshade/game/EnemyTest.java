0ad6a5bdfd81aab749bb45c093c9a6207405a1cf
Nicolas Poffley <nicolaspoffley@Nicolass-MacBook-Pro.local>
2021-03-14 16:33:41 +0000

    Added tests to SpriteTest

---
+++ src/test/java/org/nightshade/game/EnemyTest.java
@@ -0,0 +1,2 @@
+package org.nightshade.game;public class EnemyTest {
+}
--- src/test/java/org/nightshade/game/SpriteTest.java
+++ src/test/java/org/nightshade/game/SpriteTest.java
@@ -2,6 +2,7 @@ package org.nightshade.game;
 
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.nightshade.renderer.Renderer;
@@ -15,8 +16,6 @@ public class SpriteTest {
     LevelGen level;
     Game game;
     Stage tempStage;
-    Sprite sprite;
-    Image image;
 
     @Start
     public void start(Stage stage) {
@@ -24,9 +23,6 @@ public class SpriteTest {
         renderer=new Renderer();
         level = new LevelGen(120);
         tempStage = stage;
-        Image image = new Image("Grass.png");
-        sprite = new Sprite(image,20,20);
-
     }
 
     @Test
@@ -35,9 +31,55 @@ public class SpriteTest {
     }
 
     @Test
-    public void drawSprite(){
-        sprite.setPosition(0,0);
-        renderer.drawImage(image,0,0);
+    public void testDrawSprite(){
+        Image image = new Image("Grass.png");
+        Sprite sprite = new Sprite(image,40,40);
+        sprite.setPosition(100,100);
+        renderer.drawImage(image, sprite.getPositionX(), sprite.getPositionY());
     }
 
+    @Test
+    public void testIntersects(){
+        Image image = new Image("Grass.png");
+        Sprite spriteOne = new Sprite(image,40,40);
+        Sprite spriteTwo = new Sprite(image,70,40);
+        renderer.drawImage(image,0,0);
+        Assertions.assertTrue(spriteOne.intersects(spriteTwo));
+        spriteOne.setPositionX(800);
+        Assertions.assertFalse(spriteOne.intersects(spriteTwo));
+    }
+
+    @Test
+    public void testGetHeight(){
+        Image image = new Image("Grass.png");
+        //image is of height 60
+        Sprite sprite = new Sprite(image,0,0);
+        Assertions.assertEquals(60,sprite.getHeight());
+    }
+
+    @Test
+    public void testGetWidth(){
+        Image image = new Image("Grass.png");
+        //image is of width 60
+        Sprite sprite = new Sprite(image,0,0);
+        Assertions.assertEquals(60,sprite.getWidth());
+    }
+
+    @Test
+    public void testGetPositionX(){
+        Image image = new Image("Grass.png");
+        Sprite sprite = new Sprite(image,0,0);
+        Assertions.assertEquals(0,sprite.getPositionX());
+        sprite.setPositionX(50);
+        Assertions.assertEquals(50,sprite.getPositionX());
+    }
+
+    @Test
+    public void testGetPositionY(){
+        Image image = new Image("Grass.png");
+        Sprite sprite = new Sprite(image,0,0);
+        Assertions.assertEquals(0,sprite.getPositionY());
+        sprite.setPositionY(50);
+        Assertions.assertEquals(50,sprite.getPositionY());
+    }
 }
