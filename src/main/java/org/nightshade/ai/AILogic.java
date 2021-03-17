bd0a171a4637888e7f2ef7b7dd14210e50ef973c
gxb911 <gxb911@cs.bham.ac.uk>
2021-03-17 14:08:04 +0000

    reformatted AILogic.java

--- src/main/java/org/nightshade/ai/AILogic.java
+++ src/main/java/org/nightshade/ai/AILogic.java
@@ -7,21 +7,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class AILogic {
 
-    public void moveChar(AI ai, ArrayList<Sprite> platformSprites, ArrayList<Sprite> waterSprites,ArrayList<Sprite> groundSprites){
+    public void moveChar(AI ai, ArrayList<Sprite> platformSprites, ArrayList<Sprite> waterSprites, ArrayList<Sprite> groundSprites) {
 
         if (ai.getVelocity().getY() < 10) {
-            ai.setVelocity(ai.getVelocity().add(0,1));
+            ai.setVelocity(ai.getVelocity().add(0, 1));
         }
 
-        ai.moveX(ai.getSpeed(), platformSprites,groundSprites);
+        ai.moveX(ai.getSpeed(), platformSprites, groundSprites);
 
         for (Sprite platform : platformSprites) {
 
             int closeness = ThreadLocalRandom.current().nextInt(2, (30) + 1);
 
-            if(ai.getSprite().getPositionX()+ai.getSprite().getWidth()+closeness > platform.getPositionX()&&
+            if (ai.getSprite().getPositionX() + ai.getSprite().getWidth() + closeness > platform.getPositionX() &&
                     ai.getSprite().getPositionX() + 10 < platform.getPositionX() &&
-                    ai.getSprite().getPositionY() > platform.getPositionY()){
+                    ai.getSprite().getPositionY() > platform.getPositionY()) {
                 ai.jump();
             }
         }
@@ -29,20 +29,16 @@ public class AILogic {
 
             int closeness = ThreadLocalRandom.current().nextInt(2, (30) + 1);
 
-            if(ai.getSprite().getPositionX()+ai.getSprite().getWidth()+closeness > ground.getPositionX()&&
+            if (ai.getSprite().getPositionX() + ai.getSprite().getWidth() + closeness > ground.getPositionX() &&
                     ai.getSprite().getPositionX() + 10 < ground.getPositionX() &&
-                    ai.getSprite().getPositionY() > ground.getPositionY()){
+                    ai.getSprite().getPositionY() > ground.getPositionY()) {
                 ai.jump();
             }
         }
 
-        ai.moveY((int)ai.getVelocity().getY(), platformSprites,waterSprites,groundSprites);
+        ai.moveY((int) ai.getVelocity().getY(), platformSprites, waterSprites, groundSprites);
 
     }
-
-
-
-
 
 
 }
