372a6f1182ecbb1e0ba5e9f9d3389e7866849125
gxb911 <gxb911@cs.bham.ac.uk>
2021-03-17 13:56:02 +0000

    AI.java basic code reformat

--- src/main/java/org/nightshade/ai/AI.java
+++ src/main/java/org/nightshade/ai/AI.java
@@ -19,11 +19,11 @@ public class AI {
     public AI(int speed) {
         this.isAlive = true;
         this.canJump = true;
-        this.velocity = new Point2D(0,0);
+        this.velocity = new Point2D(0, 0);
         int randomXStart = ThreadLocalRandom.current().nextInt(270, 330 + 1);
         int randomYStart = ThreadLocalRandom.current().nextInt(20, 60 + 1);
 
-        this.sprite = new Sprite(new Image("view/GameComponents/AIBody.png"),randomXStart,randomYStart);
+        this.sprite = new Sprite(new Image("view/GameComponents/AIBody.png"), randomXStart, randomYStart);
         this.speed = speed;
     }
 
@@ -31,9 +31,10 @@ public class AI {
         return speed;
     }
 
-    public void displaySprite(Renderer renderer, Image image, Sprite sprite){
+    public void displaySprite(Renderer renderer, Image image, Sprite sprite) {
         renderer.drawImage(image, sprite.getPositionX(), sprite.getPositionY());
     }
+
     public void setVelocity(Point2D velocity) {
         this.velocity = velocity;
     }
@@ -61,10 +62,10 @@ public class AI {
         }
     }
 
-    public void moveX(int speed, ArrayList<Sprite> platformSprites, ArrayList<Sprite> groundSprites ) {
+    public void moveX(int speed, ArrayList<Sprite> platformSprites, ArrayList<Sprite> groundSprites) {
         int absoluteSpeed = Math.abs(speed);
 
-        for (int i = 0; i < absoluteSpeed; i ++) {
+        for (int i = 0; i < absoluteSpeed; i++) {
             for (Sprite platform : platformSprites) {
                 if (platform.intersects(sprite)) {
                     if (speed > 0) {
@@ -95,7 +96,8 @@ public class AI {
 
         }
     }
-    public void moveY(int speed,ArrayList<Sprite> platformSprites,ArrayList<Sprite> waterSprites,ArrayList<Sprite> groundSprites){
+
+    public void moveY(int speed, ArrayList<Sprite> platformSprites, ArrayList<Sprite> waterSprites, ArrayList<Sprite> groundSprites) {
 
         boolean movingDown = speed > 0;
 
