3e7ec3607833efddbdf677f810143803e3e04513
Zoe Robins <zlr901@student.bham.ac.uk>
2021-04-19 14:22:49 +0100

    added new functionality to ClientThread to send and receive Player and PlayerMoveMsg objects

--- src/main/java/org/nightshade/networking/ClientThread.java
+++ src/main/java/org/nightshade/networking/ClientThread.java
@@ -2,36 +2,34 @@ package org.nightshade.networking;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
-import org.nightshade.multiplayer.Game;
-import org.nightshade.networking.Server;
+import org.nightshade.gui.Player;
 
 public class ClientThread implements Runnable {
 
-    private BufferedReader input;
-    private PrintWriter output;
+    private final ServerLogic serverLogic;
     private int clientNo;
     private Socket socket;
-    //private GameLogic game;
-    private Game game;
-    private ClientServerController csc;
+    private ArrayList<Socket> clientSockets;
+    private ArrayList<ClientThread> clientsThreads;
+    private ArrayList<PlayerMoveMsg> moveMsgs;
+    private ArrayList<Player> players;
+
+    private ObjectOutputStream objectOutput1;
+    private ObjectInputStream objectInput1;
 
     public ClientThread(Socket client, int clientNo, ServerLogic serverLogic) {
 
-        //this.game = game;
         this.clientNo = clientNo;
         this.socket = client;
-        csc = new ClientServerController();
-        //super.setCsc(csc);
-        //super.setGame(game);
-        System.out.println(client.toString());
+        this.serverLogic = serverLogic;
+        this.moveMsgs = serverLogic.getMoveMsgs();
 
         // create the streams:
         try {
-            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), false);
-            //following line adds player to the game and tells them the goal:
-            // super.addGame(game, this); // IMPLEMENT LATER
+            objectOutput1 = new ObjectOutputStream(socket.getOutputStream());
+            objectInput1 = new ObjectInputStream(socket.getInputStream());
         } catch (IOException e) {
             System.err.println("Streams not set up for client");
         }
@@ -44,17 +42,13 @@ public class ClientThread implements Runnable {
 
         String command = "";
         try {
-            while(command != null) {
-                command = input.readLine();
-                if(command == null || command.equalsIgnoreCase("STOP")) {
-                    System.out.println("Client " + clientNo + " left");
-                } else {
-                    System.out.println("Command sent was '" + command + "' by client no. " + clientNo);
-                    outputMessage("Executing command: " + command);
-                    //super.processCommand(command, this);
-                }
+            receivePlayers();
+            sendPlayers();
+            while(true) {
+                PlayerMoveMsg moveMsg = receiveMoveMsgs();
+                sendMoveMsgs(moveMsg);
             }
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             System.out.println("Client " + clientNo + " left");
             try {
                 socket.close();
@@ -65,10 +59,59 @@ public class ClientThread implements Runnable {
 
     }
 
-    // send a message to the client:
-    protected void outputMessage(String message) {
-        csc.serverToClientMessage(output, message);
+    public void receivePlayers() {
+        Player player;
+        try {
+            player = (Player) objectInput1.readObject();
+            if(serverLogic.getPlayers().size() == 0) {
+                serverLogic.addPlayer(player);
+            } else {
+                for(int j=0; j<serverLogic.getPlayers().size(); j++) {
+                    if(serverLogic.getPlayers().get(j).getName() == player.getName()) {
+                        serverLogic.replacePlayer(j, player);
+                    } else {
+                        serverLogic.addPlayer(player);
+                    }
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    public void sendPlayers() throws IOException {
+        players = serverLogic.getPlayers();
+        System.out.println(players.size());
+        for(int i=0; i<players.size(); i++) {
+            objectOutput1.writeObject(players.get(i));
+        }
+    }
+
+    public PlayerMoveMsg receiveMoveMsgs() throws IOException, ClassNotFoundException {
+        PlayerMoveMsg moveMsg = new PlayerMoveMsg("", 0, 0, true);
+        if(moveMsg != null) {
+            moveMsg = (PlayerMoveMsg) objectInput1.readObject();
+            moveMsgs.add(moveMsg);
+            System.out.println(moveMsg.getName());
+            System.out.println(moveMsg.getX());
+            System.out.println(moveMsg.getY());
+            System.out.println(moveMsg.isAlive());
+        }
+
+        return moveMsg;
+    }
+
+    public void sendMoveMsgs(PlayerMoveMsg moveMsg) throws IOException {
+        moveMsgs = serverLogic.getMoveMsgs();
+        for(int i=0; i<moveMsgs.size(); i++) {
+            if(moveMsgs.get(i).getName() == moveMsg.getName()) {
+                moveMsgs.set(i, moveMsg);
+            }
+            objectOutput1.writeObject(moveMsgs.get(i));
+        }
+    }
+
 
 
 }
