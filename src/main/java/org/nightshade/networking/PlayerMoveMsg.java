776421a7ab3a26c3c64842c04e910fee181423f1
Zoe Robins <zlr901@student.bham.ac.uk>
2021-04-19 14:27:14 +0100

    PlayerMoveMsg now implements Serializable to send instances of it, and removed unnecessary parts

--- src/main/java/org/nightshade/networking/PlayerMoveMsg.java
+++ src/main/java/org/nightshade/networking/PlayerMoveMsg.java
@@ -1,26 +1,19 @@
 package org.nightshade.networking;
 
-//import java.io.DataInputStream;
-//import java.net.DatagramSocket;
+import java.io.Serializable;
 
-public class PlayerMoveMsg /*implements Msg */{
+public class PlayerMoveMsg implements Serializable {
 
-    private int msgType = Msg.PLAYER_MOVE_MSG;
     private String name;
     private int x;
     private int y;
     private boolean alive;
-    private Client c; // should this be a Client from multiplayer or networking ??
 
     public PlayerMoveMsg(String name, int x, int y, boolean alive) {
         this.name = name;
         this.x = x;
         this.y = y;
         this.alive = alive;
-    }
-
-    public PlayerMoveMsg(Client c) {
-        this.c = c;
     }
 
     public String getName() {
@@ -38,17 +31,5 @@ public class PlayerMoveMsg /*implements Msg */{
     public boolean isAlive() {
         return alive;
     }
-
-    /*@Override
-    public void send(DatagramSocket ds, String IP, int UDP_Port) {
-        // add code here
-
-    }
-
-    @Override
-    public void parse(DataInputStream dis) {
-        // add code here
-
-    }*/
 
 }
