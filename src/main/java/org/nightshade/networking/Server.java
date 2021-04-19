25428e0c18cac36cdb11cab3563eac93fc60dd19
Zoe Robins <zlr901@student.bham.ac.uk>
2021-04-19 12:12:32 +0100

    removed old functionality from Server and updated/tidied

--- src/main/java/org/nightshade/networking/Server.java
+++ src/main/java/org/nightshade/networking/Server.java
@@ -1,52 +1,25 @@
 package org.nightshade.networking;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import org.nightshade.gui.GuiHandler;
-import org.nightshade.multiplayer.Game;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 
 public class Server {
 
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int serverPort;
-    private static boolean created = false;
     private static ServerLogic serverLogic;
-    public static Stage serverStage;
-
-    private static int portValue;
-    //static GameLogic game = null;
-    static Game game;
-    static ServerSocket serverSocket;
 
     public Server() throws NumberFormatException, IOException {
         System.out.println("Please enter the server port number: ");
         serverPort = Integer.parseInt(br.readLine());
+        br.close();
+        serverLogic = new ServerLogic(serverPort);
     }
 
-    public static void main(String[] args) {
-        //launch(args);
-        while(!created) {
-            try {
-                Server server = new Server();
-                serverLogic = new ServerLogic(serverPort);
-                serverLogic.waitForPlayers();
-            } catch (IOException e1) {
-                System.out.println("Could not create server - check firewall");
-            } catch (NumberFormatException e2) {
-                System.out.println("Entered server port number incorrectly");
-            } finally {
-                if(serverLogic != null) {
-                    serverLogic.kill();
-                }
-            }
-        }
+    public static void main(String[] args) throws IOException {
+        new Server();
+        //serverLogic.kill();
     }
-
-
 
 }
